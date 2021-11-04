2ac8a6617072e4640f2119a41abb2aedf59a559f
Drumshtick <ekodnar@outlook.com>
2021-11-04 21:25:40 +0000

    Finished fetchCoordsByIP, project good for testing said function

--- iss.js
+++ iss.js
@@ -27,6 +27,20 @@ const fetchMyIP = function(callback) {
 };
 
 const fetchCoordsByIP = function(ip, callback) {
-
-}
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const coords = {};
+    coords["latitude"] = JSON.parse(body).latitude;
+    coords["longitude"] = JSON.parse(body).longitude;
+    callback(null, coords);
+  });
+};
 module.exports = { fetchMyIP, fetchCoordsByIP };
